5de9ecc9de240aeb854d691db2c6066c736e3d83
ShevtsovAlexander <alexander.shevtsovvv.com>
2023-12-12 18:37:36 +0300

    fix: next config  output: 'export',

--- next.config.js
+++ next.config.js
@@ -1,15 +1,35 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS || false
+
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
 
 module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
   reactStrictMode: true,
   compiler: {
     // Enables the styled-components SWC transform
     styledComponents: true
   },
-  basePath: '/github-pages',
-  images: {
-    unoptimized: true
-  }
 }
+// module.exports = {
+//   reactStrictMode: true,
+//   compiler: {
+//     // Enables the styled-components SWC transform
+//     styledComponents: true
+//   },
+//   basePath: '/github-pages',
+//   images: {
+//     unoptimized: true
+//   }
+// }
 
 
